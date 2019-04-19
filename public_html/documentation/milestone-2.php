2fcf4145d90571175b6b409e6b37c7384077dff6
Trystan Gray <trystangray7@gmail.com>
2019-04-19 11:12:38 -0600

    beginning milestone 2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p>
+			<strong> Overall Goal:</strong>
+			My goal is to have a visually appealing site using varying colors and combining it with being dynamic.
+		</p>
+		<p>
+			<strong> Welcome: </strong>
+			When the user first opens my web page i want it to feel almost soft and peaceful,
+			i will have soft colors ex: beige, cream white, soft browns paired with a beautiful
+			nature picture as the header area, below this or overlayed would be a basic intro for myself.
+			It would say "Trystan Gray; Web Developer, Designer, and explorer" or something like that.
+		</p>
+		<p>
+			<strong> About Me: </strong>
+			(should this be swapped position wise with projects?)
+			This would be the first thing they see when they scroll down, the soft colors fade into more energetic
+			colors like electric blue, candy red, ice white, etc.. This section would tell a little of my skills,
+			backstory, hobbies as well as future goals. This is the WHY part of people buying my product.
+		</p>
+		<p>
+			<strong> Projects: </strong>
+			I want the icons to flow up with the page
+			and "settle" in to their spots (this would look like the project squares have some movement to them.)
+			thye could then click on the projects for a full report on it including the actual site and code or they
+			could hover over for a brief overview. MAKE IT KNOWN YOU ARE JUST STARTING
+		</p>
+		<p>
+			<strong> Contact me: </strong>
+			in this section i will have a short statement saying something like " Contact me for any questions,
+			quotes on projects, or to schedule your project." then below is a contact form with:
+			Name, Number, email, and memo about why they're contacting me. There will also be links to
+			email me more directly than the contact form, links to my linked in and github as well.
+		</p>
+	</body>
+</html>
