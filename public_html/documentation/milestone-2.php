5c4d88f8d24659e4c3a420c95ae25ed9a348fca7
Trystan Gray <trystangray7@gmail.com>
2019-04-30 15:58:31 -0600

    grammar fixes

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,14 +7,14 @@
 	<body>
 		<h1>Content Strategy</h1>
 		<p>
-			<strong> Overall Goal:</strong>
+			<strong> Overall Goal: </strong>
 			My goal is to have a visually appealing site using varying colors and combining it with being dynamic.
 		</p>
 		<p>
 			<strong> Welcome: </strong>
 			When the user first opens my web page i want it to feel almost soft and peaceful,
 			i will have soft colors ex: beige, cream white, soft browns paired with a beautiful
-			nature picture as the header area, below this or overlayed would be a basic intro for myself.
+			nature picture as the header area, below this or over-laid would be a basic intro for myself.
 			It would say "Trystan Gray; Web Developer, Designer, and explorer" or something like that.
 		</p>
 		<p>
@@ -28,19 +28,19 @@
 			<strong> Projects: </strong>
 			I want the icons to flow up with the page
 			and "settle" in to their spots (this would look like the project squares have some movement to them.)
-			thye could then click on the projects for a full report on it including the actual site and code or they
-			could hover over for a brief overview. MAKE IT KNOWN YOU ARE JUST STARTING
+			they could then click on the projects for a full report on it including the actual site and code or they
+			could hover over for a brief overview.
 		</p>
 		<p>
 			<strong> Contact me: </strong>
-			in this section i will have a short statement saying something like " Contact me for any questions,
+			In this section I will have a short statement saying something like "Contact me for any questions,
 			quotes on projects, or to schedule your project." then below is a contact form with:
 			Name, Number, email, and memo about why they're contacting me. There will also be links to
 			email me more directly than the contact form, links to my linked in and github as well.
 		</p>
 		<p>
 			<img src="New%20Wireframe%201.png" alt="no pic 1">
-			<img src="New%20Wireframe%202.png" alt="no pic 1">
+			<img src="New%20Wireframe%202.png" alt="no pic 2">
 		</p>
 	</body>
 </html>
